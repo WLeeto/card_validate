f56505a876d66316f4820cb3aa3289ad3fd0aa7c
WLeeto <leeto4848@gmail.com>
2024-05-11 14:17:26 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./css/style.css";
 
-import "./js/app";
-import { CardValidation } from "./js/card_validation";
+import "./js/app.js";
+import { CardValidation } from "./js/card_validation.js";
 
 const cardInputElement = document.getElementById("card_number");
 const cardValidator = new CardValidation(cardInputElement);
